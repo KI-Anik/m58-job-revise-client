bce60fb66153b31c79c6514cb63ec8bdabdb248b
KI-Anik <iamkhairul101@gmail.com>
2025-03-19 10:57:51 +0600

    implement firebase

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import registerLottieData from '../../assets/lottie/register.json'
 
 const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value
+        console.log(email,password)
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,12 +20,12 @@ const Register = () => {
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <h1 className="ml-8 mt-4 text-5xl font-bold">Register now!</h1>
 
-                    <form className="card-body">
+                    <form onSubmit={handleRegister} className="card-body">
                         <fieldset className="fieldset">
                             <label className="fieldset-label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" className="input" name='email' placeholder="Email" />
                             <label className="fieldset-label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" className="input" name='password' placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Register</button>
                         </fieldset>
